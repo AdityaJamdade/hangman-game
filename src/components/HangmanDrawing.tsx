d6cbac510e3836358333a2b05a6943140a7d26f3
AdityaJamdade <adijamdade156@gmail.com>
2023-10-15 14:54:59 +0530

    hangman stand done -> moving to body elements

--- src/components/HangmanDrawing.tsx
+++ src/components/HangmanDrawing.tsx
@@ -1,5 +1,42 @@
-export function HangmanDrawing(){
+export function HangmanDrawing() {
+  return (
+    <div style={{ position: "relative" }}>
+      <div
+        style={{
+          height: "50px",
+          width: "10px",
+          background: "black",
+          position:"absolute",
+          top:0,
+          right:0,
+        }}
+      />
 
+      <div
+        style={{
+          height: "10px",
+          width: "220px",
+          marginLeft: "120px",
+          background: "black",
+        }}
+      />
 
-    return <h1>Hangman Drawing</h1>
+      <div
+        style={{
+          height: "400px",
+          width: "10px",
+          marginLeft: "120px",
+          background: "black",
+        }}
+      />
+
+      <div
+        style={{
+          height: "10px",
+          width: "250px",
+          background: "black",
+        }}
+      />
+    </div>
+  );
 }
